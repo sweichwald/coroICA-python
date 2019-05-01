961cbb25d8cbfd78c4d80a757ef80a41b7a77e73
Sebastian Weichwald <git@sweichwald.de>
2019-05-01 14:02:04 +0200

    Bugfix. Fix autocov for complex valued data.
    Signed-off-by: Sebastian Weichwald <git@sweichwald.de>

--- coroica/utils.py
+++ coroica/utils.py
@@ -10,7 +10,7 @@ def autocov(X, lag=0):
         A = X[:, lag:] - X[:, lag:].mean(axis=1, keepdims=True)
         B = X[:, :-lag] - X[:, :-lag].mean(axis=1, keepdims=True)
         n = A.shape[1] - 1
-        return A.dot(B.T) / n
+        return A.dot(B.T.conj()) / n
 
 
 def rigidgroup(length, nosamples):
